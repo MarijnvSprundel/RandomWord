16a40a5d73f7404bf305dfe2754722db6e9755b3
MarijnvSprundel <marijnvansprundel321@gmail.com>
2024-09-25 14:31:58 +0200

    Initial commit

---
+++ RandomWord/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RandomWord;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var word = "testingtesting";
+
+        var mask = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        
+        var outcome = string.Join("", word.ToCharArray().Select(x => mask.Contains(x) ? '_' : x));
+        
+        Console.WriteLine(outcome);
+
+        // for (var i = 0; i < 100; i++)
+        // {
+        //     Console.WriteLine(new WordsService().GetRandomWord());
+        // }
+    }
+}
---
+++ RandomWord/WordsService.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RandomWord;
+
+public class WordsService
+{
+    public WordsService()
+    {
+        _words = GetWords();
+    }
+    
+    private static readonly Random Random = new();
+    private List<string> _words;
+
+    public string GetRandomWord()
+    {
+        return _words[Random.Next(_words.Count)];
+    }
+
+    private List<string> GetWords()
+    {
+        return Regex.Split(File.ReadAllText(ProjectDir() + "/Words.txt"), @"\s")
+            .Where(x => !Regex.IsMatch(x, "[^a-zA-Z]"))
+            .Select(x => x.ToLower())
+            .ToList();
+    }
+
+    private string ProjectDir()
+    {
+        return Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName ??
+            throw new Exception("You fucking broke it");
+    }
+}
